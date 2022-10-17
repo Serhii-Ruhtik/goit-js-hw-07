25492f897d41de94f6a91e5dab130e524f34e0dd
Serhii-Ruhtik <fopgavrilyk1@gmail.com>
2022-10-17 22:12:13 +0300

    07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+function CreateAndRenderMarkupGallery(galleryItems) {
+
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>
+
+};
+
+
+
+
