4e935303f502a3f69a471d043f037e0f89ea151b
Serhii-Ruhtik <fopgavrilyk1@gmail.com>
2022-10-18 11:44:02 +0300

    hw-07
    картінки є у вікні , розмітку добавив

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,23 +1,26 @@
-import { galleryItems } from "./gallery-items.js";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-// console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const imgGallery = CreateAndRenderMarkupGallery(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', imgGallery);
 
 function CreateAndRenderMarkupGallery(galleryItems) {
-
-    <div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-            <img
-                class="gallery__image"
-                src="${preview}"
-                data-source="${original}"
-                alt="${description}"
-            />
-        </a>
-    </div>
-
-};
-
-
-
-
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div >
+`;
+    })
+    .join('');
+}
