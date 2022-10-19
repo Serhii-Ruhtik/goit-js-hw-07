926977bd932fc5602dfc8ff507544c0f245d030c
Serhii-Ruhtik <fopgavrilyk1@gmail.com>
2022-10-20 00:03:24 +0300

    hw-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -40,18 +40,3 @@ function onImageClick(event) {
     instance.show();
     console.log(event);
 };
-
-
-// import * as basicLightbox from 'basiclightbox'
-
-// const instance = basicLightbox.create(
-//     <div class="modal">
-//         <p>
-//             Your first lightbox with just a few lines of code.
-//             Yes, it's really that simple.
-//         </p>
-//     </div>
-// )
-
-// instance.show()
-
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,44 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const imgGallery = CreateAndRenderMarkupGallery(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', imgGallery);
+
+function CreateAndRenderMarkupGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                />
+            </a>
+        </div >
+`;
+    })
+      .join('');
+};
+
+let galleryRef = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    });
+galleryRef.on('show.simplelightbox');
+
+
+
+
+
+
+
+
+
+
+
+
